b7fd4d0774c0431dc6ef346213caf23b478fd6eb
LucaLeif <lgoeske@web.de>
2022-12-12 16:08:44 +0100

    Stand 12.12.2022 16:12

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,12 @@ namespace Day4
     class Program
     {
         static void Main(string[] args)
+        {
+            string path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName,"mock.txt");
+            int result = Challenge1(path);
+        }
+
+        private static int Challenge1(string path)
         {
 
         }
